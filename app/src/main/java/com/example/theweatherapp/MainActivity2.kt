935c198357c9c57491d175e87bc5cad6b8625d04
Thabiso67 <st10444690@vcconnect.edu.za>
2024-06-10 14:00:49 +0200

    Adjusted background image in MainActivity

--- app/src/main/java/com/example/theweatherapp/MainActivity2.kt
+++ app/src/main/java/com/example/theweatherapp/MainActivity2.kt
@@ -1,20 +1,47 @@
 package com.example.theweatherapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
     private val days = mutableListOf<String>()
-    private val min = mutableListOf<String>()
-    private val max = mutableListOf<String>()
+    private val min = mutableListOf<Int>()
+    private val max = mutableListOf<Int>()
     private val condition = mutableListOf<String>()
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val Button: Save
+        val Save: Button = findViewById(R.id.save_btn)
+        val View: Button = findViewById(R.id.view_btn)
+        val Clear: Button = findViewById(R.id.clear_btn)
+        val Exit: Button = findViewById(R.id.exit_btn)
+        val EditText: EditText = findViewById(R.id.editText)
+        val TextView: TextView = findViewById(R.id.textView3)
+        val bundle: Bundle? = intent.extras
 
+        Clear.setOnClickListener { EditText.text.clear() }
+
+        View.setOnClickListener {
+
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+
+            Exit.setOnClickListener {
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/theweatherapp/MainActivity3.kt
+++ app/src/main/java/com/example/theweatherapp/MainActivity3.kt
@@ -1,21 +1,19 @@
 package com.example.theweatherapp
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main3)
 
-        val Button: Button = findViewById(R.id.start_btn)
         val bundle: Bundle? = intent.extras
+        val Exit: Button = findViewById(R.id.exit_btn2)
 
-        Button.setOnClickListener {
+        Exit.setOnClickListener {
 
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
